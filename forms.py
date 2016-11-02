daeca7ba74a4088a5eedc50c4828f0e0021d8d35
Dennis Lorist <dennis@pexip.com>
2016-11-02 13:35:08 +1100

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,6 +1,10 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, BooleanField, PasswordField, SelectField, RadioField, IntegerField, ValidationError
 from wtforms.validators import DataRequired, Required
+import json
+import os
+
+# allow_themes = [1,2,3,4,5]
 
 class LoginForm(Form):
     user = StringField('user', validators=[DataRequired()])
@@ -16,11 +20,19 @@ class VMRForm(Form):
     def allowG_no_host_pin(form, field):
         if field.data and not form.pin.data:
             raise ValidationError('Host PIN must be set to allow guests.')
+    # def getThemeList():
+    #     with open('themes.json') as data_file:
+    #         data = json.load(data_file)
+    #         theme_choices = [ (theme["id"], theme["name"])
+    #                         for theme in data["objects"]
+    #                         if theme["id"] in allow_themes ]
+    #         return theme_choices
+
     pin = StringField('pin')
     guest_pin = StringField('Guest Pin', validators=[allowGpin, uniqueGpin])
     host_view = RadioField('Host View', choices=[('one_main_twentyone_pips', 'One Large, 21 PIPs'),('two_mains_twentyone_pips', 'Two Large, 21 PIPs'), ('one_main_zero_pips','Full screen'),('one_main_seven_pips','One Large, 7 PIPs')])
     allow_guests = BooleanField('Allow Guests', description='Allow Guests to Join', validators=[allowG_no_host_pin])
-
+    # ivr_theme = RadioField('Theme', choices=[('2', 'No Entry Tones'),('3', 'Entry Tone')])
 class MyDevices(Form):
     alias = StringField('alias')
     description = StringField('description')
