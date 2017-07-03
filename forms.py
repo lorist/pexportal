db88c4e3c26cddd90084640f677dc1060ece420e
Dennis Lorist <dennis@pexip.com>
2017-07-03 12:42:11 +1000

    Update forms.py
    Added guests_can_present

--- forms.py
+++ forms.py
@@ -17,9 +17,13 @@ class VMRForm(Form):
     def uniqueGpin(form, field):
         if field.data and field.data == form.pin.data:
             raise ValidationError('Guest PIN must be different to Host PIN.')
+
     def allowG_no_host_pin(form, field):
         if field.data and not form.pin.data:
             raise ValidationError('Host PIN must be set to allow guests.')
+    def host_guest_match_length(form, field):
+        if field.data and len(form.pin.data) != len(form.guest_pin.data):
+            raise ValidationError('Host and guest PINs must be same digit length.')
     # def getThemeList():
     #     with open('themes.json') as data_file:
     #         data = json.load(data_file)
@@ -29,10 +33,10 @@ class VMRForm(Form):
     #         return theme_choices
 
     pin = StringField('pin')
-    guest_pin = StringField('Guest Pin', validators=[allowGpin, uniqueGpin])
+    guest_pin = StringField('Guest Pin', validators=[allowGpin, uniqueGpin, host_guest_match_length])
     host_view = RadioField('Host View', choices=[('one_main_twentyone_pips', 'One Large, 21 PIPs'),('two_mains_twentyone_pips', 'Two Large, 21 PIPs'), ('one_main_zero_pips','Full screen'),('one_main_seven_pips','One Large, 7 PIPs')])
     allow_guests = BooleanField('Allow Guests', description='Allow Guests to Join', validators=[allowG_no_host_pin])
-    # ivr_theme = RadioField('Theme', choices=[('2', 'No Entry Tones'),('3', 'Entry Tone')])
+    guests_can_present = BooleanField('Guest can Present', description='Allow Guests to Present', validators=[])
 class MyDevices(Form):
     alias = StringField('alias')
     description = StringField('description')
